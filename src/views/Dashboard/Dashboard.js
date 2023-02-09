19f9cacd64d9b87db39df79cdd75fb6ae3cb114e
Arihant Agnihotri <arihantagnihotri2106@gmail.com>
2023-02-09 23:54:37 +0530

    Initial Commit

---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,393 @@
+import React, { useMemo, useState } from 'react';
+import { useWallet } from 'use-wallet';
+import moment from 'moment';
+import styled from 'styled-components';
+import Spacer from '../../components/Spacer';
+import Harvest from './components/Harvest';
+import Stake from './components/Stake';
+import { makeStyles } from '@material-ui/core/styles';
+import aveta from 'aveta';
+
+import { Box, Card, CardContent, Button, Typography, Grid } from '@material-ui/core';
+
+import UnlockWallet from '../../components/UnlockWallet';
+import Page from '../../components/Page';
+
+import useRedeemOnBoardroom from '../../hooks/useRedeemOnBoardroom';
+import useStakedBalanceOnBoardroom from '../../hooks/useStakedBalanceOnBoardroom';
+import { getDisplayBalance } from '../../utils/formatBalance';
+import useCurrentEpoch from '../../hooks/useCurrentEpoch';
+import useFetchBoardroomAPR from '../../hooks/useFetchBoardroomAPR';
+
+import useCashPriceInEstimatedTWAP from '../../hooks/useCashPriceInEstimatedTWAP';
+import useTreasuryAllocationTimes from '../../hooks/useTreasuryAllocationTimes';
+import useTotalStakedOnBoardroom from '../../hooks/useTotalStakedOnBoardroom';
+import useClaimRewardCheck from '../../hooks/boardroom/useClaimRewardCheck';
+import useWithdrawCheck from '../../hooks/boardroom/useWithdrawCheck';
+import ProgressCountdown from './components/ProgressCountdown';
+import { createGlobalStyle } from 'styled-components';
+import { Helmet } from 'react-helmet';
+
+import HomeImage from '../../assets/img/background.jpg';
+
+import CountUp from 'react-countup';
+import CardIcon from '../../components/CardIcon';
+import TokenSymbol from '../../components/TokenSymbol';
+import useBombStats from '../../hooks/useBombStats';
+import useLpStats from '../../hooks/useLpStats';
+import useLpStatsBTC from '../../hooks/useLpStatsBTC';
+import useModal from '../../hooks/useModal';
+import useZap from '../../hooks/useZap';
+import useBondStats from '../../hooks/useBondStats';
+import usebShareStats from '../../hooks/usebShareStats';
+import useTotalValueLocked from '../../hooks/useTotalValueLocked';
+// import { Bomb as bombTesting } from '../../bomb-finance/deployments/deployments.testing.json';
+//import { Bomb as bombProd } from '../../bomb-finance/deployments/deployments.mainnet.json';
+import {round, roundAndFormatNumber } from '../../0x';
+import MetamaskFox from '../../assets/img/metamask-fox.svg';
+import ZapModal from '../Bank/components/ZapModal';
+import { Alert } from '@material-ui/lab';
+import { IoCloseOutline } from 'react-icons/io5';
+import { BiLoaderAlt } from 'react-icons/bi';
+import useBombFinance from '../../hooks/useBombFinance';
+
+
+const BackgroundImage = createGlobalStyle`
+  body {
+    background: url(${HomeImage}) repeat !important;
+    background-size: cover !important;
+    background-color: #171923;
+  }
+`;
+const TITLE = 'bomb.money | Dashboard';
+
+const useStyles = makeStyles((theme) => ({
+  gridItem: {
+    height: '100%',
+    [theme.breakpoints.up('md')]: {
+      height: '90px',
+    },
+  },
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+  const { account } = useWallet();
+  const { onRedeem } = useRedeemOnBoardroom();
+  const stakedBalance = useStakedBalanceOnBoardroom();
+  const currentEpoch = useCurrentEpoch();
+  const cashStat = useCashPriceInEstimatedTWAP();
+  const totalStaked = useTotalStakedOnBoardroom();
+  const boardroomAPR = useFetchBoardroomAPR();
+  const canClaimReward = useClaimRewardCheck();
+  const canWithdraw = useWithdrawCheck();
+  const scalingFactor = useMemo(() => (cashStat ? Number(cashStat.priceInDollars).toFixed(4) : null), [cashStat]);
+  const { to } = useTreasuryAllocationTimes();
+  const TVL = useTotalValueLocked();
+  const bombFtmLpStats = useLpStatsBTC('BOMB-BTCB-LP');
+  const bShareFtmLpStats = useLpStats('BSHARE-BNB-LP');
+  const bombStats = useBombStats();
+  const bShareStats = usebShareStats();
+  const tBondStats = useBondStats();
+  const bombFinance = useBombFinance();
+
+  const buyBombAddress = //'https://app.1inch.io/#/56/swap/BTCB/BOMB';
+  //  'https://pancakeswap.finance/swap?inputCurrency=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&outputCurrency=' +
+  'https://app.bogged.finance/bsc/swap?tokenIn=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&tokenOut=0x522348779DCb2911539e76A1042aA922F9C47Ee3';
+//https://pancakeswap.finance/swap?outputCurrency=0x531780FAcE85306877D7e1F05d713D1B50a37F7A';
+const buyBShareAddress = //'https://app.1inch.io/#/56/swap/BNB/BSHARE';
+  'https://app.bogged.finance/bsc/swap?tokenIn=BNB&tokenOut=0x531780FAcE85306877D7e1F05d713D1B50a37F7A';
+const buyBusmAddress =
+  'https://app.bogged.finance/bsc/swap?tokenIn=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&tokenOut=0x6216B17f696B14701E17BCB24Ec14430261Be94A';
+const bombLPStats = useMemo(() => (bombFtmLpStats ? bombFtmLpStats : null), [bombFtmLpStats]);
+const bshareLPStats = useMemo(() => (bShareFtmLpStats ? bShareFtmLpStats : null), [bShareFtmLpStats]);
+const bombPriceInDollars = useMemo(
+  () => (bombStats ? Number(bombStats.priceInDollars).toFixed(2) : null),
+  [bombStats],
+);
+const bombPriceInBNB = useMemo(() => (bombStats ? Number(bombStats.tokenInFtm).toFixed(4) : null), [bombStats]);
+const bombCirculatingSupply = useMemo(() => (bombStats ? String(bombStats.circulatingSupply) : null), [bombStats]);
+const bombTotalSupply = useMemo(() => (bombStats ? String(bombStats.totalSupply) : null), [bombStats]);
+const bSharePriceInDollars = useMemo(
+  () => (bShareStats ? Number(bShareStats.priceInDollars).toFixed(2) : null),
+  [bShareStats],
+);
+const bSharePriceInBNB = useMemo(
+  () => (bShareStats ? Number(bShareStats.tokenInFtm).toFixed(4) : null),
+  [bShareStats],
+);
+const bShareCirculatingSupply = useMemo(
+  () => (bShareStats ? String(bShareStats.circulatingSupply) : null),
+  [bShareStats],
+);
+const bShareTotalSupply = useMemo(() => (bShareStats ? String(bShareStats.totalSupply) : null), [bShareStats]);
+
+const tBondPriceInDollars = useMemo(
+  () => (tBondStats ? Number(tBondStats.priceInDollars).toFixed(2) : null),
+  [tBondStats],
+);
+const tBondPriceInBNB = useMemo(() => (tBondStats ? Number(tBondStats.tokenInFtm).toFixed(4) : null), [tBondStats]);
+const tBondCirculatingSupply = useMemo(
+  () => (tBondStats ? String(tBondStats.circulatingSupply) : null),
+  [tBondStats],
+);
+const tBondTotalSupply = useMemo(() => (tBondStats ? String(tBondStats.totalSupply) : null), [tBondStats]);
+
+const bombLpZap = useZap({ depositTokenName: 'BOMB-BTCB-LP' });
+const bshareLpZap = useZap({ depositTokenName: 'BSHARE-BNB-LP' });
+
+const [onPresentBombZap, onDissmissBombZap] = useModal(
+  <ZapModal
+    decimals={18}
+    onConfirm={(zappingToken, tokenName, amount) => {
+      if (Number(amount) <= 0 || isNaN(Number(amount))) return;
+      bombLpZap.onZap(zappingToken, tokenName, amount);
+      onDissmissBombZap();
+    }}
+    tokenName={'BOMB-BTCB-LP'}
+  />,
+);
+
+const [onPresentBshareZap, onDissmissBshareZap] = useModal(
+  <ZapModal
+    decimals={18}
+    onConfirm={(zappingToken, tokenName, amount) => {
+      if (Number(amount) <= 0 || isNaN(Number(amount))) return;
+      bshareLpZap.onZap(zappingToken, tokenName, amount);
+      onDissmissBshareZap();
+    }}
+    tokenName={'BSHARE-BNB-LP'}
+  />,
+);
+
+const [modal, setModal] = useState(false);
+const [videoLoading, setVideoLoading] = useState(true);
+
+const openModal = () => {
+  setModal(!modal);
+};
+
+const spinner = () => {
+  setVideoLoading(!videoLoading);
+};
+
+  return (
+    <Page>
+      <BackgroundImage />
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+          <Typography color="textPrimary" align="center" variant="h3" gutterBottom>
+            Dashboard
+          </Typography>
+          <Box mt={5}>
+            <Grid container justifyContent="center" spacing={3}>
+              <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                <Card className={classes.gridItem}>
+                  <CardContent style={{textAlign: 'center' }}>
+                    <Typography style={{textTransform: 'uppercase', color: '#f9d749' }}>Next Epoch</Typography>
+                    <ProgressCountdown base={moment().toDate()} hideBar={true} deadline={to} description="Next Epoch" />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                <Card className={classes.gridItem}>
+                  <CardContent align="center">
+                    <Typography style={{ textTransform: 'uppercase', color: '#f9d749' }}>Current Epoch</Typography>
+                    <Typography>{Number(currentEpoch)}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                <Card className={classes.gridItem}>
+                  <CardContent align="center">
+                    <Typography style={{ textTransform: 'uppercase', color: '#f9d749' }}>
+                      BOMB PEG <small>(TWAP)</small>
+                    </Typography>
+                    <Typography>{scalingFactor} BTC</Typography>
+                    <Typography>
+                      <small>per 10,000 BOMB</small>
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                <Card className={classes.gridItem}>
+                  <CardContent align="center">
+                    <Typography style={{ textTransform: 'uppercase', color: '#f9d749' }}>APR</Typography>
+                    <Typography>{boardroomAPR.toFixed(2)}%</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+              <Card className={classes.gridItem}>
+              <CardContent align="center">
+              <Typography style={{ textTransform: 'uppercase', color: '#f9d749' }}>
+                      Total value locked 
+                    </Typography>
+              {/* <CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" /> */}
+              <Typography> $ {aveta(TVL)}</Typography>
+              </CardContent>
+              </Card>
+              </Grid>
+            </Grid>
+
+            {/* <Grid container justify="center" spacing={3}>
+            <Grid item xs={4}>
+              <Card>
+                <CardContent align="center">
+                  <Typography>Rewards</Typography>
+
+                </CardContent>
+                <CardActions style={{justifyContent: 'center'}}>
+                  <Button color="primary" variant="outlined">Claim Reward</Button>
+                </CardActions>
+                <CardContent align="center">
+                  <Typography>Claim Countdown</Typography>
+                  <Typography>00:00:00</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card>
+                <CardContent align="center">
+                  <Typography>Stakings</Typography>
+                  <Typography>{getDisplayBalance(stakedBalance)}</Typography>
+                </CardContent>
+                <CardActions style={{justifyContent: 'center'}}>
+                  <Button>+</Button>
+                  <Button>-</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid> */}
+          </Box>
+          <Box mt={5}>
+          <Grid container justifyContent="center" spacing={3}>
+            {/* BOMB */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BOMB" />
+                </CardIcon>
+              </Box>
+              <Button
+                onClick={() => {
+                  bombFinance.watchAssetInMetamask('BOMB');
+                }}
+                style={{ position: 'absolute', top: '10px', right: '10px', border: '1px grey solid' }}
+              >
+                {' '}
+                <b>+</b>&nbsp;&nbsp;
+                <img alt="metamask fox" style={{ width: '20px', filter: 'grayscale(100%)' }} src={MetamaskFox} />
+              </Button>
+              <h2 style={{ marginBottom: '10px' }}>BOMB</h2>
+              10,000 BOMB (1.0 Peg) =
+              <Box>
+                <span style={{ fontSize: '30px', color: 'white' }}>
+                  {bombPriceInBNB ? bombPriceInBNB : '-.----'} BTC
+                </span>
+              </Box>
+              <Box>
+                <span style={{ fontSize: '16px', alignContent: 'flex-start' }}>
+                  ${bombPriceInDollars ? roundAndFormatNumber(bombPriceInDollars, 2) : '-.--'} / BOMB
+                </span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${roundAndFormatNumber(bombCirculatingSupply * bombPriceInDollars, 2)} <br />
+                Circulating Supply: {aveta(round(bombCirculatingSupply, 2), {precision :2})} <br />
+                Total Supply: {aveta(round(bombTotalSupply, 2), {precision :2})}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* BSHARE */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <Button
+                onClick={() => {
+                  bombFinance.watchAssetInMetamask('BSHARE');
+                }}
+                style={{ position: 'absolute', top: '10px', right: '10px', border: '1px grey solid' }}
+              >
+                {' '}
+                <b>+</b>&nbsp;&nbsp;
+                <img alt="metamask fox" style={{ width: '20px', filter: 'grayscale(100%)' }} src={MetamaskFox} />
+              </Button>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BSHARE" />
+                </CardIcon>
+              </Box>
+              <h2 style={{ marginBottom: '10px' }}>BSHARE</h2>
+              Current Price
+              <Box>
+                <span style={{ fontSize: '30px', color: 'white' }}>
+                  {bSharePriceInBNB ? bSharePriceInBNB : '-.----'} BNB
+                </span>
+              </Box>
+              <Box>
+                <span style={{ fontSize: '16px' }}>
+                  ${bSharePriceInDollars ? bSharePriceInDollars : '-.--'} / BSHARE
+                </span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${roundAndFormatNumber((bShareCirculatingSupply * bSharePriceInDollars).toFixed(2), 2)}{' '}
+                <br />
+                Circulating Supply: {aveta(round(bShareCirculatingSupply, 2), {precision :2})} <br />
+                Total Supply: {aveta(round(bShareTotalSupply, 2), {precision :2})}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* BBOND */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <Button
+                onClick={() => {
+                  bombFinance.watchAssetInMetamask('BBOND');
+                }}
+                style={{ position: 'absolute', top: '10px', right: '10px', border: '1px grey solid' }}
+              >
+                {' '}
+                <b>+</b>&nbsp;&nbsp;
+                <img alt="metamask fox" style={{ width: '20px', filter: 'grayscale(100%)' }} src={MetamaskFox} />
+              </Button>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BBOND" />
+                </CardIcon>
+              </Box>
+              <h2 style={{ marginBottom: '10px' }}>BBOND</h2>
+              10,000 BBOND
+              <Box>
+                <span style={{ fontSize: '30px', color: 'white' }}>
+                  {tBondPriceInBNB ? tBondPriceInBNB : '-.----'} BTC
+                </span>
+              </Box>
+              <Box>
+                <span style={{ fontSize: '16px' }}>${tBondPriceInDollars ? tBondPriceInDollars : '-.--'} / BBOND</span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: ${roundAndFormatNumber((tBondCirculatingSupply * tBondPriceInDollars).toFixed(2), 2)} <br />
+                Circulating Supply: {aveta(round(tBondCirculatingSupply, 2), {precision :2})} <br />
+                Total Supply: {aveta(round(tBondTotalSupply, 2), {precision :2})}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+        </Grid>
+
+          </Box>
+
+    </Page>
+  );
+};
+
+export default Dashboard;
